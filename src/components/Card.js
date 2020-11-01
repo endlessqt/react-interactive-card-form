5cfc63c6089e3bc0440491a0d05784ff3fba0bb7
Anton Martynov <nocturne6667@gmail.com>
2020-11-02 02:04:17 +0300

    add flip animation on card-label

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useFormContext, useWatch } from "react-hook-form";
+import "./Card.scss";
+import chip from "../assets/images/card-chip.png";
+import masterCard from "../assets/images/master-card.png";
+import visa from "../assets/images/visa.png";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
 const Card = () => {
   const { control } = useFormContext();
   const cardNum = useWatch({
@@ -12,13 +17,52 @@ const Card = () => {
     name: "cardHolder",
     defaultValue: "John Doe",
   });
+  const [card, setCard] = useState("visa");
+  let ref = useRef(null);
+  useEffect(() => {
+    if (cardNum.startsWith("54")) {
+      setCard("master");
+    } else {
+      setCard("visa");
+    }
+  }, [cardNum]);
   return (
-    <div>
-      Here Card Image With Values
-      <div>Card Number is {cardNum}</div>
-      <div>Card Holder is {cardHolder}</div>
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-inner__top">
+          <div className="card-inner__chip">
+            <img className="card-inner__chip-image" src={chip} alt="chip"></img>
+          </div>
+          <SwitchTransition>
+            <CSSTransition
+              key={card}
+              nodeRef={ref}
+              timeout={500}
+              addEndListener={(node, done) => {
+                node = ref.current;
+                node.addEventListener("transitionend", done, false);
+              }}
+              classNames="flip"
+            >
+              <div className="card-inner__card-label">
+                <img
+                  src={card === "visa" ? visa : masterCard}
+                  className="card-inner__card-label-image"
+                  alt="card-label"
+                  ref={ref}
+                />
+              </div>
+            </CSSTransition>
+          </SwitchTransition>
+        </div>
+        <div>center</div>
+        <div>bottom</div>
+      </div>
     </div>
   );
 };
+//<div>Card Number is {cardNum}</div>
+
+/* <div>Card Holder is {cardHolder}</div> */
 
 export default Card;
